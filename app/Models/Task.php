fa93cb4bc2496b5503ab910634769e5765e7e50f
Grizzly182 <tiger.mananticot@yandex.ru>
2023-06-17 18:54:00 +0300

    created models and relations between them

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    public function checklist(): BelongsTo
+    {
+        return $this->belongsTo(Checklist::class, 'checklist_id', 'id');
+    }
+
+    public function steps(): HasMany
+    {
+        return $this->hasMany(Step::class, 'task_id', 'id');
+    }
+}
