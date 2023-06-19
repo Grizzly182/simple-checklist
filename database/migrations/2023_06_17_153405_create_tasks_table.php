1e814d852ea5ae4c9a83d6b99f77142e15a7ced7
Grizzly182 <tiger.mananticot@yandex.ru>
2023-06-19 14:39:10 +0300

    forgot to add is_active flag

--- database/migrations/2023_06_17_153405_create_tasks_table.php
+++ database/migrations/2023_06_17_153405_create_tasks_table.php
@@ -16,6 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string('name', 128);
+            $table->boolean('is_active')->default('0');
             $table->foreignId('checklist_id')->references('id')->on('checklists')->cascadeOnDelete();
             $table->timestamps();
         });
